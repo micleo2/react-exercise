6b98c7ddd85b4766fd62e02f4e6e78f0ba2eac70
Michael Leon <mal7@illinois.edu>
2018-09-17 18:17:22 -0500

    Finished up to part 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ class App extends Component {
     return (
       <div className="app">
         <h1>Hello World!</h1>
-        <Instructions />
+        <Instructions
+          shouldDisplayImage="true"
+          items={['hello', 'world', 'there', 'is', 'an', 'array']}
+        />
       </div>
     )
   }
--- src/components/Instructions.component.js
+++ src/components/Instructions.component.js
@@ -2,15 +2,29 @@ import React, { Component } from 'react'
 import './../styles/instructions.css'
 
 class Instructions extends Component {
+  constructor(props) {
+    super(props)
+    this.shouldDisplayImage = props.shouldDisplayImage
+    this.list = props.items.filter(item => item.length >= 3)
+  }
+
   render() {
+    const shouldDisplayImage = this.shouldDisplayImage
     return (
       <div className="instructions">
         Follow the instructions on the README to get started!
-        <img
-          className="instructions__logo"
-          src="https://uiuc.hack4impact.org/img/colored-logo.png"
-          alt="h4i logo"
-        />
+        {shouldDisplayImage === 'true' && (
+          <img
+            className="instructions__logo"
+            src="https://uiuc.hack4impact.org/img/colored-logo.png"
+            alt="h4i logo"
+          />
+        )}
+        <ul>
+          {this.list.map((item, index) => (
+            <li>{index % 2 == 0 ? item : item.toUpperCase()}</li>
+          ))}
+        </ul>
       </div>
     )
   }
